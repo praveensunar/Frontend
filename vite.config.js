005293097339d628d58a36805be0fb109224bc97
praveensunar <140327889+praveensunar@users.noreply.github.com>
2025-03-20 20:41:05 +0530

    q

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,13 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: ['frontend-oefe.onrender.com']
+  }
 })
